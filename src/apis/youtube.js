a06f21be1265cc70589eb0dce54efc6d03608f97
Zach Ioannou <zioannou77@gmail.com>
2020-01-16 00:01:31 +0200

    added axios youtube api component helper

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAhyNfTJYlOJqmV9LYzOh6dscgyUo5Oork';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});

bc948a7270d98dfc3dfc40383d864057811cdd69
Zach Ioannou <zioannou77@gmail.com>
2020-01-15 23:51:39 +0200

    added props callback

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,16 @@ import SearchBar from './SearchBar';
 import ErrorBoundary from './ErrorBoundary';
 
 class App extends React.Component {
+  onFormSubmit = term => {
+    console.log(term);
+  };
+
   render() {
     return (
-      <div>
-        <SearchBar />
+      <div className="ui container">
+        <ErrorBoundary>
+          <SearchBar onSubmit={this.onFormSubmit} />
+        </ErrorBoundary>
       </div>
     );
   }

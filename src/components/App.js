55aca32a6a1e7ff49cfab4d62719088e7a519892
Zach Ioannou <zioannou77@gmail.com>
2020-01-17 23:30:46 +0200

    added vides list

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube, { baseParams } from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
   state = { videos: [] };
@@ -20,7 +21,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have{' '}
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ src/components/VideoList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideosList = ({ videos }) => {
+  return videos.length;
+};
+
+export default VideosList;

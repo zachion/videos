bbe83d2423a2e7bc5f32e398539dec9b439707df
Zach Ioannou <zioannou77@gmail.com>
2020-01-18 01:31:14 +0200

    restyled VideoItems component

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,13 +1,17 @@
+import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
+    <div class="video-item item">
       <img
+        className="ui image"
         src={video.snippet.thumbnails.medium.url}
         alt={video.snippet.title}
       />
-      {video.snippet.title}
+      <div className="content">
+        <div class="header">{video.snippet.title}</div>
+      </div>
     </div>
   );
 };
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,9 +2,10 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideosList = ({ videos }) => {
-  return videos.map((video, index) => {
+  const renderedList = videos.map((video, index) => {
     return <VideoItem key={index} video={video} />;
   });
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideosList;
